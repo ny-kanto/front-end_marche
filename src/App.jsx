65d34100ea9b62e60acf241cb7896feb71c0e37f
Ny Kanto RANDRIAMBOLOLONA <120046452+randria-nykanto@users.noreply.github.com>
2024-08-18 11:17:21 +0300

    Fixed url

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {  HashRouter as Router, Route, Routes } from 'react-router-dom';
+import {  BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignupForm from './pages/auth/SignupForm';
 import LoginForm from './pages/auth/LoginForm';
 import ListProduit from './pages/produit/ListProduit';
