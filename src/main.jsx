9c7012c0db61ad9464fe4c92a57941ecd71dde2e
Ny Kanto RANDRIAMBOLOLONA <120046452+randria-nykanto@users.noreply.github.com>
2024-08-18 12:33:13 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LoginForm from './pages/auth/LoginForm.jsx'
 import SignupForm from './pages/auth/SignupForm.jsx'
 import ListProduit from './pages/produit/ListProduit.jsx'
 import DetailProduit from './pages/produit/DetailProduit.jsx'
 import Error from './components/Error.jsx'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/front-end_marche/",
     element: <LoginForm />,
     children: [
       {
-        path: "login",
+        path: "/front-end_marche/login",
         element: <LoginForm />,
       },
       {
-        path: "signup",
+        path: "/front-end_marche/signup",
         element: <SignupForm />,
       },
       {
-        path: "product/list",
+        path: "/front-end_marche/product/list",
         element: <ListProduit />,
       },
       {
-        path: "product/:id",
+        path: "/front-end_marche/product/:id",
         element: <DetailProduit />,
       },
       {
-        path: "error/403",
+        path: "/front-end_marche/error/403",
         element: <Error errorCode="403" title="Forbidden" message="Access is denied" redirectLink="/front-end_marche/login" />,
       },
       {
